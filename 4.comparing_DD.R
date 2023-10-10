8104b68bde38f590e42ae6e00df6d4d6fc1a7ed3
FMestre1 <mestre.frederico@gmail.com>
2023-10-10 10:17:24 +0100

    add script 4

---
+++ 4.comparing_DD.R
@@ -0,0 +1,25 @@
+################################################################################
+################################################################################
+#           SCRIPT 4 - COMPARING THE DD WITH KNOWN DISTRIBUTIONS
+################################################################################
+################################################################################
+
+#FMestre
+#09-10-2022
+
+#Acording to our last meeting Miguel is of the opinion that we should compare 
+#both DD wit known distributions
+
+#Degree distributions
+overall_degree_distribution_list
+in_degree_distribution_list
+out_degree_distribution_list
+
+#Use these networks
+final_data_frame_16_FW$network_number
+final_data_frame_16_MUT$network_number
+
+
+
+
+
