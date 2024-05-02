83e8c5bcb62b5c26c1f25514a0bcea2740517e75
FMestre1 <mestre.frederico@gmail.com>
2024-05-02 11:42:24 +0100

    02-05-2024

--- script5_after_review.R
+++ script5_after_review.R
@@ -84,6 +84,112 @@ reference_list$Reference <- stringr::str_remove(reference_list$Reference, "\\[1\
 #                   cv.fold = 10,
 #                   step = 1) # ERROR!!
 
+
+#### START - NOT USED
+
+#From: https://gist.github.com/sillasgonzaga/eef0577c14b83b32f9b7cc480d2765dd
+plot_rf_tree <- function(final_model, tree_num, shorten_label = TRUE) {
+  
+  library(tidyr)
+  library(dplyr)
+  library(igraph)
+  library(ggraph)
+  
+  # source: https://shiring.github.io/machine_learning/2017/03/16/rf_plot_ggraph
+  
+  # get tree by index
+  tree <- randomForest::getTree(final_model, 
+                                k = tree_num, 
+                                labelVar = TRUE) %>%
+    tibble::rownames_to_column() %>%
+    # make leaf split points to NA, so the 0s won't get plotted
+    mutate(`split point` = ifelse(is.na(prediction), `split point`, NA))
+  
+  # prepare data frame for graph
+  graph_frame <- data.frame(from = rep(tree$rowname, 2),
+                            to = c(tree$`left daughter`, tree$`right daughter`))
+  
+  # convert to graph and delete the last node that we don't want to plot
+  graph <- graph_from_data_frame(graph_frame) %>%
+    delete_vertices("0")
+  
+  # set node labels
+  V(graph)$node_label <- gsub("_", " ", as.character(tree$`split var`))
+  
+  if (shorten_label) {
+    V(graph)$leaf_label <- substr(as.character(tree$prediction), 1, 1)
+  }
+  
+  V(graph)$split <- as.character(round(tree$`split point`, digits = 2))
+  
+  # plot
+  plot <- ggraph(graph, 'tree') + 
+    theme_graph() +
+    geom_edge_link() +
+    geom_node_point() +
+    geom_node_label(aes(label = leaf_label, fill = leaf_label), na.rm = TRUE, 
+                    repel = FALSE, colour = "white",
+                    show.legend = FALSE)
+  
+  print(plot)
+}
+
+#From: https://www.r-bloggers.com/2017/03/plotting-trees-from-random-forest-models-with-ggraph/
+tree_func <- function(final_model, tree_num) {
+  
+  library(dplyr)
+  library(ggraph)
+  library(igraph)
+  
+  # get tree by index
+  tree <- randomForest::getTree(final_model, 
+                                k = tree_num, 
+                                labelVar = TRUE) %>%
+    tibble::rownames_to_column() %>%
+    # make leaf split points to NA, so the 0s won't get plotted
+    mutate(`split point` = ifelse(is.na(prediction), `split point`, NA))
+  
+  # prepare data frame for graph
+  graph_frame <- data.frame(from = rep(tree$rowname, 2),
+                            to = c(tree$`left daughter`, tree$`right daughter`))
+  
+  # convert to graph and delete the last node that we don't want to plot
+  graph <- graph_from_data_frame(graph_frame) %>%
+    delete_vertices("0")
+  
+  # set node labels
+  V(graph)$node_label <- gsub("_", " ", as.character(tree$`split var`))
+  V(graph)$leaf_label <- as.character(tree$prediction)
+  V(graph)$split <- as.character(round(tree$`split point`, digits = 2))
+  
+  # plot
+  plot <- ggraph(graph, 'dendrogram') + 
+    theme_bw() +
+    geom_edge_link() +
+    geom_node_point() +
+    geom_node_text(aes(label = node_label), na.rm = TRUE, repel = TRUE) +
+    geom_node_label(aes(label = split), vjust = 2.5, na.rm = TRUE, fill = "white") +
+    geom_node_label(aes(label = leaf_label, fill = leaf_label), na.rm = TRUE, 
+                    repel = TRUE, colour = "white", fontface = "bold", show.legend = FALSE) +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.background = element_blank(),
+          plot.background = element_rect(fill = "white"),
+          panel.border = element_blank(),
+          axis.line = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          plot.title = element_text(size = 18))
+  
+  print(plot)
+}
+
+#### END - NOT USED
+
+
 ##### Random Forest #####
 
 # 1.1. Food webs
